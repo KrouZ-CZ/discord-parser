b7ab035c604d254938eb148e2f831041ba5e5fa2
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2022-03-05 15:17:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import requests
 import json
-headers = {"authorization": "YOUR TOKEN HERE"}
-
+headers = {
+    "authorization": "YOUR TOKEN HERE"
+}
 all_messages = []
 def main():
     lists = 0
-    id = int('LAST MESSAGE ID')
     chid = int('CHAT ID')
     try:
+        url = f"https://discord.com/api/v9/channels/{chid}/messages"
         while True:
-            url = f"https://discord.com/api/v9/channels/{chid}/messages?before={id}&limit=100"
             r = requests.get(url=url, headers=headers)
             text = json.loads(r.text)
             if len(text) == 0:
-                print("All messages has parsed")
-                break
+                print("All messages has parsed"); break
             for item in text:
                 if item['content'] != "":
                     all_messages.insert(0, {item['author']['username']: item['content']})
                 id = item['id']
             print(f"Lists {lists + 1} has parsed")
             lists += 1
+            url = f"https://discord.com/api/v9/channels/{chid}/messages?before={id}&limit=100"
     except:
         print('Operation except')
-    with open('data.json', "w", encoding='utf-8') as file:
+    with open('ServerForBrother.json', "w", encoding='utf-8') as file:
         json.dump(all_messages, file, indent=4, ensure_ascii=False)
     print('All data saved!')
 if __name__ == "__main__":
